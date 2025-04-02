eb7273827d32eafdf56aa49bb310318bd6c77506
shiv122 <37039611+shiv122@users.noreply.github.com>
2025-04-03 00:14:57 +0530

    added composer install

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
 
+# Ensure Composer dependencies are installed
+composer install --no-dev --optimize-autoloader
+
 # Set permissions for Laravel directories
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
-# permissions for PHPMyAdmin
+# Permissions for PHPMyAdmin
 mkdir -p /sessions
-
 chmod 777 /sessions
 
 exec "$@"
